b5089baa84cc3ae7787bd3458dc8d39f0fe8f624
jiadaMizan <jiada1308@gmail.com>
2026-09-25 11:17:09 +0700

    11th commit

--- src/main/java/id/ac/polinema/oop/Customer.java
+++ src/main/java/id/ac/polinema/oop/Customer.java
@@ -42,7 +42,7 @@ public class Customer {
      * @param name new name
      */
     public void setName(String newName) {
-        if (name != null) {
+        if (newName != null ) {
             this.name = newName;
         } else {
             return;
